b15044cdb61837ccdb0aa305b1da9eed4d71035b
gianlucadr <gianlucadallerive@gmail.com>
2024-11-29 15:44:52 +0000

    Update4

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,9 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+select * from Customers where FirstName='Cleo' and LastName='Goldwater';
+
+Select * from Dishes where name ='Quinoa Salmon Salad';
+
+UPDATE Customers SET FavoriteDish=(Select DishID from Dishes where name ='Quinoa Salmon Salad')
+WHERE FirstName='Cleo' and LastName='Goldwater';
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,9 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+Select COUNT(Orders.OrderID) as OrderCount, Customers.FirstName, Customers.LastName, Customers.Email 
+from Orders inner join
+Customers on Orders.CustomerID=Customers.CustomerID
+group by Customers.CustomerID
+order by OrderCount DESC
+limit 10;
