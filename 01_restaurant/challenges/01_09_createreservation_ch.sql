eb5b6030b2e2c05e146d1aa85d42e2fd0779ca57
gianlucadr <gianlucadallerive@gmail.com>
2024-11-29 15:33:56 +0000

    Update3

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,13 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+Select * from Customers where Email='smac@kinetecoinc.com';
+
+INSERT INTO Customers (FirstName, LastName, Email) VALUES ('Sam','McAdams','smac@kinetecoinc.com');
+
+
+
+Insert INTO Reservations (CustomerID,Date,PartySize)
+VALUES((Select CustomerID from Customers where email='smac@kinetecoinc.com'),'2022-08-12 18:00:00',5);
+
+Select * from Reservations order by ReservationID DESC;
--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,24 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+select * from Customers where FirstName='Loretta' and LastName='Hundey';
+Select * from Dishes where Name='House Salad';
+Select * from Dishes where Name='Mini Cheeseburgers';
+Select * from Dishes where Name='Tropical Blue Smoothie';
+
+Select * from Orders order by OrderID desc;
+Insert into ORDERS (CustomerID,OrderDate) VALUES ((select CustomerID from Customers 
+  where FirstName='Loretta' and LastName='Hundey'),null);
+
+SELECT * from OrdersDishes order by OrdersDishesID DESC;
+
+Insert into OrdersDishes (OrderID, DishID) VALUES (1001,(Select DishID from Dishes where Name='House Salad'));
+Insert into OrdersDishes (OrderID, DishID) VALUES (1001,(Select DishID from Dishes where Name='Mini Cheeseburgers'));
+Insert into OrdersDishes (OrderID, DishID) VALUES (1001,(Select DishID from Dishes where Name='Tropical Blue Smoothie'));
+
+SELECT SUM(Dishes.Price), OrdersDishes.OrderID
+from OrdersDishes inner join Dishes on
+OrdersDishes.DishID=Dishes.DishID
+where OrdersDishes.OrderID=1001
+group by OrdersDishes.OrderID;
